3637576a02c272fad036d0e81ab9f5ecdb0983a6
AmLpR06 <amalrajan8509@gmail.com>
2024-10-23 00:20:55 +0530

    edith next.config.mjs

--- cynosylix_official_website/next.config.mjs
+++ cynosylix_official_website/next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+/* eslint-disable import/no-anonymous-default-export */
+// next.config.mjs
+export default {
+    async redirects() {
+      return [
+        {
+          source: '/:path*',  // Matches all paths
+          destination: '/',  // Redirect to the index.html
+          permanent: true,  // Status code 308 (permanent redirect)
+        },
+      ]
+    },
+  }
+  
