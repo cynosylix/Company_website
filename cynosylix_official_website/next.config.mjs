c9a0b4845e01ea81a00184cc6b4014b3f4f0562e
AmLpR06 <amalrajan8509@gmail.com>
2024-10-24 19:38:43 +0530

    final

--- cynosylix_official_website/next.config.mjs
+++ cynosylix_official_website/next.config.mjs
@@ -1,5 +1,4 @@
 /* eslint-disable import/no-anonymous-default-export */
-
 // next.config.mjs
 export default {
   distDir: 'build', // Custom build directory
@@ -8,9 +7,12 @@ export default {
     return [
       {
         source: '/:path*',  // Matches all paths
-        destination: '/',   // Redirect to the index.html
+        destination: '/',   // Redirect all paths to homepage
         permanent: true,    // Status code 308 (permanent redirect)
       },
     ]
   },
+
+  // Set base path if your site is served from a subdirectory on the domain
+  // basePath: '/subdirectory', 
 }
