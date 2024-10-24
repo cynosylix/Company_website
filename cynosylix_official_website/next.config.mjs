25f4cc0fa2cd51fa7fa38f1580bde07561836fa8
AmLpR06 <amalrajan8509@gmail.com>
2024-10-24 12:43:49 +0530

    final out

--- cynosylix_official_website/next.config.mjs
+++ cynosylix_official_website/next.config.mjs
@@ -1,14 +1,16 @@
 /* eslint-disable import/no-anonymous-default-export */
+
 // next.config.mjs
 export default {
-    async redirects() {
-      return [
-        {
-          source: '/:path*',  // Matches all paths
-          destination: '/',  // Redirect to the index.html
-          permanent: true,  // Status code 308 (permanent redirect)
-        },
-      ]
-    },
-  }
-  
+  distDir: 'build', // Custom build directory
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',  // Matches all paths
+        destination: '/',   // Redirect to the index.html
+        permanent: true,    // Status code 308 (permanent redirect)
+      },
+    ]
+  },
+}
